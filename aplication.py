284301188972017217951a062b29702dfa5147da
gmatiascr <gmatiascr62@gmail.com>
2023-07-13 18:01:25 -0300

    cambio precio

--- aplication.py
+++ aplication.py
@@ -28,7 +28,7 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=50)
 directorio = os.path.abspath(os.path.dirname(__file__))
 
 # precio del material de una rueda de trommel sin tapas y sin iva
-pmrt = 236000
+pmrt = 23600
 
 def escribir(seniores, direc, transp, lugar, cui, iba, remitos, dia, mes, anio, orden, cantidad, desc, cantidad_dos, desc_dos, cantidad_tres, desc_tres, cantidad_cuatro, desc_cuatro, cantidad_cinco,desc_cinco, cantidad_seis, desc_seis):
     remit = remitos
